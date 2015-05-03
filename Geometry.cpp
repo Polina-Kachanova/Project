74d72750ffa0bf499db497e131614cc98237c3c0
Polina-Kachanova <Pol_Ka@tut.by>
2015-05-04 00:28:26 +0300

    Update Geometry.cpp

--- Geometry.cpp
+++ Geometry.cpp
@@ -1,11 +1,6 @@
-#include <vector>
-#include <cassert>
-#include <cmath>
-#include <iostream>
 #include "geometry.h"
 
-template <> template <> Vec3<int>::Vec3<>(const Vec3<float> &v) :  x(int(v.x + .5)), y(int(v.y + .5)), z(int(v.z + .5)) {
-}
-
-template <> template <> Vec3<float>::Vec3<>(const Vec3<int> &v) : x(v.x), y(v.y), z(v.z) {
-}
+template <> template <> vec<3, int>  ::vec(const vec<3, float> &v) : x(int(v.x + .5f)), y(int(v.y + .5f)), z(int(v.z + .5f)) {}
+template <> template <> vec<3, float>::vec(const vec<3, int> &v) : x(v.x), y(v.y), z(v.z) {}
+template <> template <> vec<2, int>  ::vec(const vec<2, float> &v) : x(int(v.x + .5f)), y(int(v.y + .5f)) {}
+template <> template <> vec<2, float>::vec(const vec<2, int> &v) : x(v.x), y(v.y) {}
